89706da2d1a7b172c1205cea154b76f85c6e55b1
David R. Williamson <drwill@microsoft.com>
2020-06-11 14:57:35 -0700

    feat(adt): track2 control plane SDK

---
+++ sdk/digitaltwins/Azure.ResourceManager.DigitalTwins/tests/DigitalTwinInstanceLifecycleTests.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Azure.ResourceManager.DigitalTwins.Models;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.DigitalTwins.Tests
+{
+    public class DigitalTwinInstanceLifecycleTests : E2eTestBase
+    {
+        public DigitalTwinInstanceLifecycleTests(bool isAsync)
+            : base(isAsync)
+        {
+        }
+
+        [Test]
+        public async Task DigitalTwinInstance_Lifecycle()
+        {
+            string dtInstanceName = $"DtInstanceLifecycle_{Rand.Next(1000)}";
+
+            try
+            {
+                DigitalTwinsCreateOrUpdateOperation createResponse = await Client
+                    .StartCreateOrUpdateAsync(
+                        TestEnvironment.ResourceGroup,
+                        dtInstanceName,
+                        new DigitalTwinsDescription(TestEnvironment.Location))
+                    .ConfigureAwait(false);
+
+                createResponse.GetRawResponse().Status.Should().Be(201);
+
+                Response<DigitalTwinsDescription> createdResponse = await WaitForCompletionAsync(createResponse).ConfigureAwait(false);
+                createdResponse.GetRawResponse().Status.Should().Be(200);
+
+                DigitalTwinsDescription createdDtInstance = createdResponse.Value;
+                createdDtInstance.Name.Should().Be(dtInstanceName);
+                createdDtInstance.Location.Should().Be(TestEnvironment.Location);
+
+            }
+            finally
+            {
+                DigitalTwinsDeleteOperation deleteResponse = await Client
+                    .StartDeleteAsync(
+                        TestEnvironment.ResourceGroup,
+                        dtInstanceName)
+                    .ConfigureAwait(false);
+
+                deleteResponse.GetRawResponse().Status.Should().Be(202);
+
+                Response<Response> deletedResponse = await WaitForCompletionAsync(deleteResponse).ConfigureAwait(false);
+                deletedResponse.GetRawResponse().Status.Should().Be(204);
+            }
+        }
+    }
+}
---
+++ sdk/digitaltwins/Azure.ResourceManager.DigitalTwins/tests/E2eTestBase.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+
+namespace Azure.ResourceManager.DigitalTwins.Tests
+{
+    [ClientTestFixture]
+    public abstract class E2eTestBase : RecordedTestBase<DigitalTwinsManagementTestEnvironment>
+    {
+        private static readonly TimeSpan ZeroPollingInterval = TimeSpan.FromSeconds(0);
+
+        protected DigitalTwinsManagementClient MainClient { get; private set; }
+        protected DigitalTwinsClient Client { get; private set; }
+        protected Random Rand { get; private set; }
+
+        protected E2eTestBase(bool isAsync)
+            : base(isAsync)
+        {
+            MainClient = GetClient();
+            Client = MainClient.GetDigitalTwinsClient();
+        }
+
+        protected E2eTestBase(bool isAsync, RecordedTestMode mode)
+            : base(isAsync, mode)
+        {
+            MainClient = GetClient();
+            Client = MainClient.GetDigitalTwinsClient();
+        }
+
+        protected DigitalTwinsManagementClient GetClient()
+        {
+            return CreateClient<DigitalTwinsManagementClient>(
+                TestEnvironment.SubscriptionId,
+                TestEnvironment.Credential);
+        }
+
+        protected ValueTask<Response<T>> WaitForCompletionAsync<T>(Operation<T> operation)
+        {
+            if (Mode == RecordedTestMode.Playback)
+            {
+                return operation.WaitForCompletionAsync(ZeroPollingInterval, default);
+            }
+
+            return operation.WaitForCompletionAsync();
+        }
+    }
+}
